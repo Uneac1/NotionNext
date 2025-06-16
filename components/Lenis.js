83ca64291d039a7fee4e8c84cf9bb37891f4d75f
Uneac1 <1006849568@qq.com>
2025-06-16 11:05:38 +0800

    Update Lenis.js

--- components/Lenis.js
+++ components/Lenis.js
@@ -24,7 +24,7 @@ const Lenis = () => {
 
         // 创建 Lenis 实例
         const lenis = new Lenis({
-          duration: 1.2,
+          duration: 0.2,
           easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
           direction: 'vertical', // vertical, horizontal
           gestureDirection: 'vertical', // vertical, horizontal, both
