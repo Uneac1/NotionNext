2a404fa4fb34a566b42fe7b565f3e550acf6ea33
Uneac1 <1006849568@qq.com>
2025-06-16 17:07:49 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'y130.icu',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '消耗品',
+  HEO_HERO_TITLE_2: '所有的男人都是',
+  HEO_HERO_TITLE_3: '村上龙',
+  HEO_HERO_TITLE_4: 'like tears in rain',
+  HEO_HERO_TITLE_5: 'All those moments will be lost in time',
+  HEO_HERO_TITLE_LINK: 'https://ows.y130.icu/PIN',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
