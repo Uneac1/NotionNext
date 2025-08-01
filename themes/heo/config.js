3733e1cd55cacad84d5ea42bef05a1008e207a59
Uneac1 <1006849568@qq.com>
2025-08-01 17:35:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -68,7 +68,7 @@ const CONFIG = {
     },
     {
       title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      img_1: '/images/heo/7009.png',
       color_1: '#57b6e6',
       title_2: 'Photoshop',
       img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
