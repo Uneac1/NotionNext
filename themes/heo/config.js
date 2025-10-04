e582d1b73b302eaa652c313a908bed73795ee19b
Uneac1 <1006849568@qq.com>
2025-10-04 11:49:59 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,7 +51,7 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/Uneac1',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
   HEO_INFO_CARD_URL3: 'https://y130.icu',
   HEO_INFO_CARD_TEXT3: '了解更多',
